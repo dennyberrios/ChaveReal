f8ff3cda94b3d8bf392b08ba5792b1afe8ca47af
Danny Berrios <denny_berrios@hotmail.com>
2025-02-14 18:52:52 -0300

    Importando a secreen PromotionsPage para a rota Promotions

---
+++ src/app/promotions/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import PromotionsPage from "@/screens/Promotions";
+
+const Promotions = () => {
+    return ( 
+        <PromotionsPage />
+     );
+}
+ 
+export default Promotions;
